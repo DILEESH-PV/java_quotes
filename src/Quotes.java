e4373b32580d523b827cfd4c1b01bb982dbd955d
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-05 11:46:35 +0530

    updated cred operations

--- src/Quotes.java
+++ src/Quotes.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.util.Scanner;
 
 public class Quotes
@@ -5,7 +6,7 @@ public class Quotes
     public static void main(String[] args)
     {
         int choice;
-        String name,des,pb,incr,cat;
+        String name,quo;
 
         Scanner sc=new Scanner(System.in);
         while (true)
@@ -22,23 +23,102 @@ public class Quotes
             switch(choice)
             {
                 case 1:
-                    System.out.println("Add author selected");
+                    System.out.println("Enter the author name");
+                    name=sc.next();
+                    System.out.println("Enter the quotes");
+                    quo=sc.next();
+                    System.out.println("Data inserted successfully");
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/quotedb","root","");
+                        String sql="INSERT INTO `quotes`(`quote`, `author`) VALUES (?,?)";
+                        PreparedStatement stmt=con.prepareStatement(sql);
+                        stmt.setString(1,quo);
+                        stmt.setString(2,name);
+                        stmt.executeUpdate();
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println(e);}
 
 
                     break;
                 case 2:
                     System.out.println("view all authors selected");
+                    try{
 
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/quotedb","root","");
+                        String sql="SELECT * FROM `quotes`";
+                        Statement stmt=con.createStatement();
+                        ResultSet rs=stmt.executeQuery(sql);
+                        while(rs.next()){
+                            String getQuo=rs.getString("quote");
+                            String getName=rs.getString("author");
+                            System.out.println("Quotes       : "+getQuo);
+                            System.out.println("Author Name  : "+getName+"\n");
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println(e);}
                     break;
                 case 3:
                     System.out.println("search a author selected");
+                    System.out.println("Enter the author name for searching");
+                    String au=sc.next();
+                    try{
+
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/quotedb","root","");
+                        String sql="SELECT `quote`, `author` FROM `quotes` WHERE `author`='"+au+"'";
+                        Statement stmt=con.createStatement();
+                        ResultSet rs=stmt.executeQuery(sql);
+                        while(rs.next()){
+                            String getQuo=rs.getString("quote");
+                            String getName=rs.getString("author");
+                            System.out.println("Quotes       : "+getQuo);
+                            System.out.println("Author Name  : "+getName+"\n");
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println(e);}
 
                     break;
                 case 4:
                     System.out.println("update author selected");
+                    System.out.println("Enter the author name");
+                    String aut=sc.next();
+                    System.out.println("Enter the quotes to be updated");
+                    quo=sc.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quotedb", "root", "");
+                        String sql = "UPDATE `quotes` SET `quote`='"+quo+"' WHERE `author`='"+aut+"'";
+                        Statement stmt = con.createStatement();
+                        stmt.executeUpdate(sql);
+                        System.out.println("updated successfully");
+                    }
+                    catch (Exception e){
+                        System.out.println(e);}
+
                     break;
                 case 5:
-                    System.out.println("Delete a author selected");
+                    System.out.println("Enter the author for deleting data");
+                    String auth=sc.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quotedb", "root", "");
+                        String sql = "DELETE FROM `quotes` WHERE `author`='"+auth+"'";
+                        Statement stmt = con.createStatement();
+                        stmt.executeUpdate(sql);
+                        System.out.println("Deleted successfully");
+                    }
+                    catch (Exception e){
+                        System.out.println(e);}
 
                     break;
                 case 6:
